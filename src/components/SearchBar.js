9b523ab10d5905dd2901589440fcbf01e5b37d7f
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-04 13:26:05 -0500

    prevent user from hitting enter on text field

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -18,7 +18,7 @@ export default function SearchBar(props) {
 			noValidate
 			autoComplete="off"
 		>
-			<TextField id="outlined-basic" label="pokemon..." variant="outlined" onChange={props.handleSearch} />
+			<TextField id="outlined-basic" label="pokemon..." variant="outlined" onChange={props.handleSearch} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}/>
 		</Box>
 	);
 }
