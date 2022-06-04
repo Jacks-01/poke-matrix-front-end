8c8bad1fad8a089649013472781fc664dfa99b66
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-04 11:20:39 -0500

    pushing mvp to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 /**
  * @file Main.js
- * @description this component is responsible for holding all content in the home page (pokedex).
+ * @description Contains user data to send to DB and update favorites.
  */
 import React, { Component } from 'react';
 import AllPokemon from './AllPokemon';
@@ -14,51 +14,62 @@ class Main extends Component {
 				_id: '629a888c84190fc0d6ecbedc',
 				username: 'jack',
 				password: 'jack',
-				favorites: []
-			}
-		}
+				favorites: [],
+			},
+		};
 	}
-
 
 	addToUserFavorites = async (pokemon) => {
 		// add to user pokemon favorite array
-		console.log(`this is our pokemon marked as favorite in main.js ${pokemon.name}`)
+		console.log(
+			`this is our pokemon marked as favorite in main.js ${pokemon.name}`
+		);
 		this.state.user.favorites.push(pokemon);
-		console.log(`this is our favorites array after adding a new one: ${this.state.user.favorites}`);
+		console.log(
+			`this is our favorites array after adding a new one: ${this.state.user.favorites}`
+		);
 		this.updateUser(this.state.user);
-	}
+	};
 
 	removeFromUserFavorites = async (pokemon) => {
 		//removes a favorite pokemon from user favorite array
 		let updatedPokemon = this.state.user.favorites.filter((element) => {
-			return (element.name !== pokemon.name);
+			return element.name !== pokemon.name;
 		});
 		console.log(`this is updatedPokemon: ${updatedPokemon}`);
-		this.setState({favorites: updatedPokemon});
-		console.log(`our updated pokemon array after removing ${pokemon.name} from favorites: ${JSON.stringify(updatedPokemon)}`)
+		this.setState({ favorites: updatedPokemon });
+		console.log(
+			`our updated pokemon array after removing ${
+				pokemon.name
+			} from favorites: ${JSON.stringify(updatedPokemon)}`
+		);
 		let updatedUser = {
 			_id: this.state.user._id,
 			username: this.state.user.username,
 			password: this.state.user.password,
-			favorites: updatedPokemon
+			favorites: updatedPokemon,
 		};
 		this.updateUser(updatedUser);
 	};
 
 	updateUser = async (updatedUser) => {
-
-		console.log(`user before updateUser: ${updatedUser.favorites}`)
-		await axios.patch(`http://localhost:3001/users/${updatedUser._id}`, updatedUser)
-				.then((res) => {
-					console.log(res);
-				}).catch((err) => {
-					console.error(err)
-				});
-	}
+		console.log(`user before updateUser: ${updatedUser.favorites}`);
+		await axios
+			.patch(`http://localhost:3001/users/${updatedUser._id}`, updatedUser)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
 	render() {
 		return (
 			<div>
-				<AllPokemon addToUserFavorites={this.addToUserFavorites} removeFromUserFavorites={this.removeFromUserFavorites} />
+				<AllPokemon
+					addToUserFavorites={this.addToUserFavorites}
+					removeFromUserFavorites={this.removeFromUserFavorites}
+				/>
 			</div>
 		);
 	}
