5587c651320ed70b2868efda97e9532b6260850a
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-04 14:13:40 -0500

    styled the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,21 @@
  */
 
 import React, { Component } from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
-
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 class Header extends Component {
 	render() {
 		return (
 			<>
 				<Navbar bg="dark" variant="dark">
-						<Nav className="me-auto justify-content-center">
-							<Nav.Link href="#home">About</Nav.Link>
-							<Nav.Link href="#features">Login</Nav.Link>
-						</Nav>
+					<Container className='justify-content-center'>
+						<Navbar.Brand href="#home">Poke-Matrix</Navbar.Brand>
+					
+					<Nav className='me auto'>
+						<Nav.Link href="#home">About</Nav.Link>
+						<Nav.Link href="#login">Login</Nav.Link>
+					</Nav>
+					</Container>
 				</Navbar>
 			</>
 		);
